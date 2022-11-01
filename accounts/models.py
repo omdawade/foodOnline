d0dca50008debecbee1e2bce444994ca3b32f498
unknown <omdawade@gmail.com>
2022-11-01 09:28:14 +0530

    vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -40,11 +40,11 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
 
     ROLE_CHOICE = (
-        (RESTAURANT, 'restaurant'),
+        (VENDOR, 'vendor'),
         (CUSTOMER, 'Customer'),
     )
     first_name = models.CharField(max_length=50)
